87906ec220c60d4cbcb0377fbe193aaf47a16029
Kelly Garzon <kelly.garzon@viamericas.com>
2021-05-25 17:07:29 -0500

    upd

--- Connection/SqlConnection.js
+++ Connection/SqlConnection.js
@@ -3,9 +3,9 @@ const sql = require('mssql')
 
 // configuration to connect to sql, the database is on rds on aws
 const config = {  
-user: "admin",  
-password: "Huequitos.08",  
-server: "statsdb.cfl8tbxohjth.us-east-2.rds.amazonaws.com",  
+user: "xxxxx",  
+password: "xxxxxx",  
+server: "statsdb.xxxxxx.us-east-2.rds.amazonaws.com",  
 port:1433,
 database: "adnmutant",
 options: {
